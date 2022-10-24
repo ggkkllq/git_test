8a2ee469913683ddb5996737fc63d3ecf3bf9f98
kjq <851941506@qq.com>
2022-10-24 20:52:55 +0800

    hot-fix commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("2222");
         System.out.println("hot-fix");
+        System.out.println("hot-fix test");
     }
 }
